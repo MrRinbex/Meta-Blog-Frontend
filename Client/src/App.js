fb2e8670f55fcb24e3a12fb66e5b876af50b1690
Léo Cheffi <contact@leocheffi.com>
2022-10-10 16:42:47 +0200

    create pages and router dom

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,51 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Test home page</h1>
+      </div>
+    ),
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/single",
+    element: <Single />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <p>
-        hola amigos
-      </p>
+      <RouterProvider router={router} />
     </>
-
   );
 }
 
