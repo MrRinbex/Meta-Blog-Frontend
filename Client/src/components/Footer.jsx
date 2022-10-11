5e3fbf986f89112aa47699707842c37a769d194f
Léo Cheffi <contact@leocheffi.com>
2022-10-11 17:38:09 +0200

    footer add and favicon

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Logo from "../img/logoMetaBlog.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={Logo} alt="logo" />
+      <span>© 2022 Leocheffi.com</span>
+    </footer>
+  );
 };
 
 export default Footer;
