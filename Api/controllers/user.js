9a548fb891e4674bae3390a4d309acd00b051c57
Léo Cheffi <contact@leocheffi.com>
2022-10-15 01:05:38 +0200

    create delete user and update user all CRUD work fine

--- Api/controllers/user.js
+++ Api/controllers/user.js
@@ -20,3 +20,52 @@ export const getUser = (req, res) => {
     return res.status(200).json(data[0]);
   });
 };
+
+//UPDATE USER
+
+export const updateUser = (req, res) => {
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not Authenticated!");
+
+  const key = process.env.JWT_KEY;
+
+  jwt.verify(token, key, (err, userInfo) => {
+    if (err) return res.status(403).json("Not valid Token");
+
+    const userId = req.params.id;
+    const q = "UPDATE users SET `username`=?,`email`=?,`img`=? WHERE id=? ";
+
+    const values = [req.body.username, req.body.email, req.body.img];
+
+    database.query(q, [...values, userId, userInfo.id], (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json(data);
+      }
+      return res.json("User has been updated !").status(201);
+    });
+  });
+};
+
+//DELETE USER
+
+export const deleteUser = (req, res) => {
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not Authenticated!");
+
+  const key = process.env.JWT_KEY;
+
+  jwt.verify(token, key, (err, userInfo) => {
+    if (err) return res.status(403).json("Not valid Token");
+
+    const userId = req.params.id;
+    const q = "DELETE FROM users WHERE id=?";
+
+    database.query(q, [userId, userInfo.id], (err, data) => {
+      if (err) {
+        return res.status(403).json(err);
+      }
+      return res.json("User deleted");
+    });
+  });
+};
--- Api/routes/users.js
+++ Api/routes/users.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { getUsers, getUser } from "../controllers/user.js";
+import {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.get("/:id", getUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
